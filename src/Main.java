283450fe37c0583506ba1963d932544ca1b4591a
Obverser <57118333+Obverser@users.noreply.github.com>
2022-11-01 15:53:23 -0700

    Adding cocktail sort and some organization

--- src/Main.java
+++ src/Main.java
@@ -1,118 +1,118 @@
 import java.util.Random;
 
 public class Main {
-    StopWatch sw = new StopWatch();
+    static StopWatch sw = new StopWatch();
     public static final int sizeOfArrays = 15;
 
-    public static void main (String[] args){
-        Main main = new Main();
-        main.run();
+    public static void main(String[] args) {
+        int[] arrayForBubble = createArray("bubble");
+        sw.start();
+        bubbleSort(arrayForBubble);
+        sw.stop();
+        System.out.println(" Sorted bubble: " + displayArray(arrayForBubble));
+        System.out.println("  Time taken for bubble (ns): " + sw.getElapsedTime());
+
+
+        int[] arrayForSelection = createArray("selection");
+        sw.start();
+        selectionSort(arrayForSelection);
+        sw.stop();
+        System.out.println(" Sorted selection: " + displayArray(arrayForBubble));
+        System.out.println("  Time taken for selection (ns): " + sw.getElapsedTime());
+
+
+        int[] arrayForQuick = createArray("quick");
+        sw.start();
+        quickSort(arrayForQuick, 0, sizeOfArrays-1);
+        sw.stop();
+        System.out.println(" Sorted quick: " + displayArray(arrayForQuick));
+        System.out.println("  Time taken for quick (ns): " + sw.getElapsedTime());
+
+        int[] arrayForCocktail = createArray("cocktail");
+        sw.start();
+        cocktailSort(arrayForCocktail); // Not cocktail shaker!!
+        sw.stop();
+        System.out.println(" Sorted cocktail: " + displayArray(arrayForCocktail));
+        System.out.println("  Time taken for cocktail (ns): " + sw.getElapsedTime());
+
+        int[] arrayForHeap = createArray("heap");
+        sw.start();
+        heapSort(arrayForHeap);
+        sw.stop();
+        System.out.println(" Sorted heap: " + displayArray(arrayForHeap));
+        System.out.println("  Time taken for heap (ns): " + sw.getElapsedTime());
+
+        int[] arrayForMerge = createArray("merge");
+        sw.start();
+        mergeSort(arrayForMerge, 0, sizeOfArrays-1);
+        sw.stop();
+        System.out.println(" Sorted merge: " + displayArray(arrayForMerge));
+        System.out.println("  Time taken for merge (ns): " + sw.getElapsedTime());
     }
 
-    private void run() {
-        int[] arrayForBubble = createArray("Bubble sort");
-        sw.start();
-        BubbleSort(arrayForBubble);
-        sw.stop();
-        System.out.println("Sorted bubble: " + displayArray(arrayForBubble));
-        System.out.println("Time taken for bubble: " + sw.getElapsedTime());
-
-
-        int[] arrayForSelection = createArray("Selection Sort");
-        sw.start();
-        SelectionSort(arrayForSelection);
-        sw.stop();
-        System.out.println("Sorted selection: " + displayArray(arrayForBubble));
-        System.out.println("Time taken for selection: " + sw.getElapsedTime());
-
-
-        int[] arrayForQuicksort = createArray("QuickSort");
-        sw.start();
-        quickSort(arrayForQuicksort, 0, sizeOfArrays-1);
-        sw.stop();
-        System.out.println("Sorted QuickSort: " + displayArray(arrayForQuicksort));
-        System.out.println("Time taken for quick: " + sw.getElapsedTime());
-
-        /**
-        int[] arrayForCocktail = createArray("CockTail Sort");
-        CocktailSort(arrayForCocktail);
-
-        int[] arrayForHeapsort = createArray("HeapSort");
-        HeapSort(arrayForHeapsort);
-        */
-
-        int[] arrayForMergesort = createArray("MergeSort");
-        sw.start();
-        MergeSort(arrayForMergesort, 0, sizeOfArrays-1);
-        sw.stop();
-        System.out.println("Sorted MergeSort: " + displayArray(arrayForMergesort));
-        System.out.println("Time taken for merge: " + sw.getElapsedTime());
-
-        }
-        private void MergeSort(int arr[], int l, int r) {
+    static void mergeSort(int arr[], int l, int r) {
         if (l < r) {
             // Find the middle point
             int m = l + (r - l) / 2;
 
             // Sort first and second halves
-            MergeSort(arr, l, m);
-            MergeSort(arr, m + 1, r);
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
 
             // Merge the sorted halves
-            Merge(arr, l, m, r);
-            }
+            merge(arr, l, m, r);
         }
+    }
 
-        private void Merge(int[] arr, int l, int m, int r) {
-            // Find sizes of two subarrays to be merged
-            int n1 = m - l + 1;
-            int n2 = r - m;
+    static void merge(int[] arr, int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
-            /* Create temp arrays */
-            int L[] = new int[n1];
-            int R[] = new int[n2];
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
 
-            /*Copy data to temp arrays*/
-            for (int i = 0; i < n1; ++i)
-                L[i] = arr[l + i];
-            for (int j = 0; j < n2; ++j)
-                R[j] = arr[m + 1 + j];
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
 
-            /* Merge the temp arrays */
+        /* Merge the temp arrays */
 
-            // Initial indexes of first and second subarrays
-            int i = 0, j = 0;
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
 
-            // Initial index of merged subarray array
-            int k = l;
-            while (i < n1 && j < n2) {
-                if (L[i] <= R[j]) {
-                    arr[k] = L[i];
-                    i++;
-                }
-                else {
-                    arr[k] = R[j];
-                    j++;
-                }
-                k++;
-            }
-
-            /* Copy remaining elements of L[] if any */
-            while (i < n1) {
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
                 arr[k] = L[i];
                 i++;
-                k++;
-            }
-
-            /* Copy remaining elements of R[] if any */
-            while (j < n2) {
+            } else {
                 arr[k] = R[j];
                 j++;
-                k++;
             }
+                k++;
         }
 
-    public static String displayArray(int[] myArray) {
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    static String displayArray(int[] myArray) {
         StringBuilder build = new StringBuilder();
         build.append('[');
         for (int number: myArray){
@@ -120,10 +120,11 @@ public class Main {
         }
         build.delete(build.lastIndexOf(","), build.length());
         build.append(']');
+
         return build.toString();
     }
 
-    private int[] createArray(String sort) {
+    static int[] createArray(String sort) {
         Random rd = new Random(); // creating Random object
         int[] arr = new int[sizeOfArrays];
         for (int i = 0; i < sizeOfArrays; i++) {
@@ -131,16 +132,45 @@ public class Main {
             // random object.(maxval - minval)+1) + minval for the bounds of the random
         }
         System.out.println("Array before " + sort + ": " + displayArray(arr));
+    
         return arr;
     }
 
-    public void HeapSort(int[] array) {
+    static void heapSort(int[] array) {
+        // TODO: I don't even know what this is
     }
 
-    public void CocktailSort(int[] array) {
+    static void cocktailSort(int[] array) {
+        int start = 0;
+        int end = array.length;
+        boolean swap = false;
+
+        do {
+            for (int i = start; i < end - 1; ++i) {
+                if (array[i] > array[i + 1]) {
+                    // Store and swap
+                    int temp = array[i];
+                    array[i] = array[i + 1]; array[i + 1] = temp;
+                    swap = true;
+                }
+            }
+
+            if (!swap) { break; } else { swap = !swap; } // Already sorted
+
+            end--;
+            for (int i = end - 1; i >= start; i--) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1]; array[i + 1] = temp;
+                    swap = true;
+                }
+            }
+
+            start++;
+        } while (swap);
     }
 
-    public void quickSort(int[] array, int low, int high) {
+    static void quickSort(int[] array, int low, int high) {
         if (low < high) {
             // pi is partitioning index, arr[p]
             // is now at right place
@@ -151,7 +181,8 @@ public class Main {
             quickSort(array, pi + 1, high);
         }
     }
-    private int partition(int[] arr, int low, int high) {
+
+    static int partition(int[] arr, int low, int high) {
         // pivot
         int pivot = arr[high];
 
@@ -176,14 +207,14 @@ public class Main {
         return (i + 1);
     }
 
-    private void swap(int[] arr, int i, int j) {
+    static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
 
-    public void SelectionSort(int[] array) {
+    static void selectionSort(int[] array) {
         for (int i = 0; i < Main.sizeOfArrays - 1; i++) {
             // Find the minimum element in unsorted array
             int min_idx = i;
@@ -199,7 +230,7 @@ public class Main {
         }
     }
 
-    public void BubbleSort(int[] array) {
+    static void bubbleSort(int[] array) {
         // loop to access each array element
         for (int i = 0; i < sizeOfArrays - 1; i++)
 
@@ -218,6 +249,7 @@ public class Main {
                 }
     }
 }
+
 class StopWatch {
     private long startTime;
     private long endTime;
